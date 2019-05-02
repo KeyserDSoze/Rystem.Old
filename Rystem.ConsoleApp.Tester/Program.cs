1afff54809ec9778167ec98577bd663c2b255e88
Alessandro Rapiti <a.rapiti@vetrya.com>
2019-05-02 14:49:05 +0200

    start

--- Rystem.ConsoleApp.Tester/Program.cs
+++ Rystem.ConsoleApp.Tester/Program.cs
@@ -6,7 +6,7 @@ namespace Rystem.ConsoleApp.Tester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello WorldX!");
         }
     }
 }
