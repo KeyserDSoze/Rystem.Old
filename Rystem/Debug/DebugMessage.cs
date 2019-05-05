b787eb06f0618044bd2d2da4c96ebe558bc1f313
Alessandro Rapiti <a.rapiti@vetrya.com>
2019-05-06 00:30:43 +0200

    start

---
+++ Rystem/Debug/DebugMessage.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rystem.Debug
+{
+    public class DebugMessage
+    {
+        private static int counter = 0;
+        private static object TrafficLight = new object();
+        private int? id;
+        public int Id
+        {
+            get
+            {
+                if (id != null) return id ?? 0;
+                lock (TrafficLight)
+                {
+                    id = counter++;
+                }
+                return id ?? 0;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public int DelayInSeconds { get; set; }
+        public EventData EventData { get; set; }
+        public Message Message { get; set; }
+        public DebugMessage()
+        {
+            int a = this.Id;
+        }
+        public override string ToString()
+        {
+            return $"Id: {this.Id} - Delay: {this.DelayInSeconds} s - Has EventData: {this.EventData != null} - Has Message: {this.Message != null}";
+        }
+    }
+}
