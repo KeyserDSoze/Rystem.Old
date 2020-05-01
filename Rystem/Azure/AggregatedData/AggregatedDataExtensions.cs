8300a591462bc10c1043850a382fdf8619fd95c9
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-05-01 17:26:58 +0200

    Checkpoint refactoring per togliere dai manager la reflection

--- Rystem/Azure/AggregatedData/AggregatedDataExtensions.cs
+++ Rystem/Azure/AggregatedData/AggregatedDataExtensions.cs
@@ -13,51 +13,50 @@ namespace System
     {
         private static Dictionary<string, IAggregatedDataManager> Managers = new Dictionary<string, IAggregatedDataManager>();
         private readonly static object TrafficLight = new object();
-        private static IAggregatedDataManager Manager(Type dataType)
+        private static IAggregatedDataManager Manager<TEntity>()
+            where TEntity : IAggregatedData
         {
-            if (!Managers.ContainsKey(dataType.FullName))
+            string name = typeof(TEntity).FullName;
+            if (!Managers.ContainsKey(name))
                 lock (TrafficLight)
-                    if (!Managers.ContainsKey(dataType.FullName))
-                    {
-                        Type genericType = typeof(AggregatedDataManager<>).MakeGenericType(dataType);
-                        Managers.Add(dataType.FullName, (IAggregatedDataManager)Activator.CreateInstance(genericType));
-                    }
-            return Managers[dataType.FullName];
+                    if (!Managers.ContainsKey(name))
+                        Managers.Add(name, new AggregatedDataManager<TEntity>());
+            return Managers[name];
         }
         public static async Task<bool> WriteAsync<TEntity>(this TEntity entity, long offset = 0, Installation installation = Installation.Default)
-        where TEntity : IAggregatedData
-       => await Manager(entity.GetType()).WriteAsync(entity, installation, offset);
+            where TEntity : IAggregatedData
+            => await Manager<TEntity>().WriteAsync(entity, installation, offset);
         public static async Task<bool> DeleteAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).DeleteAsync(entity, installation);
+           => await Manager<TEntity>().DeleteAsync(entity, installation);
         public static async Task<bool> ExistsAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).ExistsAsync(entity, installation);
+           => await Manager<TEntity>().ExistsAsync(entity, installation);
         public static async Task<TEntity> FetchAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).FetchAsync<TEntity>(entity, installation);
+           => await Manager<TEntity>().FetchAsync<TEntity>(entity, installation);
         public static async Task<IEnumerable<TEntity>> ListAsync<TEntity>(this TEntity entity, string prefix = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).ListAsync<TEntity>(entity, installation, prefix, takeCount);
+           => await Manager<TEntity>().ListAsync<TEntity>(entity, installation, prefix, takeCount);
         public static async Task<IList<string>> SearchAsync<TEntity>(this TEntity entity, string prefix = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).SearchAsync(entity, installation, prefix, takeCount);
+           => await Manager<TEntity>().SearchAsync(entity, installation, prefix, takeCount);
         public static async Task<IList<AggregatedDataDummy>> FetchPropertiesAsync<TEntity>(this TEntity entity, string prefix = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => await Manager(entity.GetType()).FetchPropertiesAsync(entity, installation, prefix, takeCount);
+           => await Manager<TEntity>().FetchPropertiesAsync(entity, installation, prefix, takeCount);
 
         public static bool Write<TEntity>(this TEntity entity, long offset = 0, Installation installation = Installation.Default)
-        where TEntity : IAggregatedData
-       => WriteAsync(entity, offset, installation).ConfigureAwait(false).GetAwaiter().GetResult();
+            where TEntity : IAggregatedData
+            => WriteAsync(entity, offset, installation).ConfigureAwait(false).GetAwaiter().GetResult();
         public static TEntity Fetch<TEntity>(this TEntity entity, Installation installation = Installation.Default)
-       where TEntity : IAggregatedData
-      => FetchAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
+           where TEntity : IAggregatedData
+           => FetchAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
         public static bool Delete<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => DeleteAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
+            => DeleteAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
         public static bool Exists<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
-           => ExistsAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
+            => ExistsAsync(entity, installation).ConfigureAwait(false).GetAwaiter().GetResult();
         public static IEnumerable<TEntity> List<TEntity>(this TEntity entity, string prefix = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : IAggregatedData
            => ListAsync<TEntity>(entity, prefix, takeCount, installation).ConfigureAwait(false).GetAwaiter().GetResult();
@@ -69,6 +68,6 @@ namespace System
            => FetchPropertiesAsync(entity, prefix, takeCount, installation).ConfigureAwait(false).GetAwaiter().GetResult();
         public static string GetName<TEntity>(this TEntity entity, Installation installation = Installation.Default)
            where TEntity : IAggregatedData
-       => Manager(entity.GetType()).GetName(installation);
+            => Manager<TEntity>().GetName(installation);
     }
 }
--- Rystem/Azure/NoSql/NoSqlExtensions.cs
+++ Rystem/Azure/NoSql/NoSqlExtensions.cs
@@ -13,35 +13,34 @@ namespace System
     {
         private static Dictionary<string, INoSqlManager> Managers = new Dictionary<string, INoSqlManager>();
         private readonly static object TrafficLight = new object();
-        private static INoSqlManager Manager(Type messageType)
+        private static INoSqlManager Manager<TEntity>()
+            where TEntity : INoSql
         {
-            if (!Managers.ContainsKey(messageType.FullName))
+            string name = typeof(TEntity).FullName;
+            if (!Managers.ContainsKey(name))
                 lock (TrafficLight)
-                    if (!Managers.ContainsKey(messageType.FullName))
-                    {
-                        Type genericType = typeof(NoSqlManager<>).MakeGenericType(messageType);
-                        Managers.Add(messageType.FullName, (INoSqlManager)Activator.CreateInstance(genericType));
-                    }
-            return Managers[messageType.FullName];
+                    if (!Managers.ContainsKey(name))
+                        Managers.Add(name, new NoSqlManager<TEntity>());
+            return Managers[name];
         }
         public static async Task<bool> UpdateAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entity.GetType()).UpdateAsync(entity, installation);
+           => await Manager<TEntity>().UpdateAsync(entity, installation);
         public static async Task<bool> UpdateBatchAsync<TEntity>(this IEnumerable<TEntity> entities, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entities.FirstOrDefault().GetType()).UpdateBatchAsync(entities.Select(x => (INoSql)x), installation);
+           => await Manager<TEntity>().UpdateBatchAsync(entities.Select(x => (INoSql)x), installation);
         public static async Task<bool> DeleteAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entity.GetType()).DeleteAsync(entity, installation);
+           => await Manager<TEntity>().DeleteAsync(entity, installation);
         public static async Task<bool> DeleteBatchAsync<TEntity>(this IEnumerable<TEntity> entities, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entities.FirstOrDefault().GetType()).DeleteBatchAsync(entities.Select(x => (INoSql)x), installation);
+           => await Manager<TEntity>().DeleteBatchAsync(entities.Select(x => (INoSql)x), installation);
         public static async Task<bool> ExistsAsync<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entity.GetType()).ExistsAsync(entity, installation);
+           => await Manager<TEntity>().ExistsAsync(entity, installation);
         public static async Task<IList<TEntity>> GetAsync<TEntity>(this TEntity entity, Expression<Func<TEntity, bool>> expression = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : INoSql
-           => await Manager(entity.GetType()).GetAsync(entity, installation, expression, takeCount);
+           => await Manager<TEntity>().GetAsync(entity, installation, expression, takeCount);
 
         public static bool Update<TEntity>(this TEntity entity, Installation installation = Installation.Default)
            where TEntity : INoSql
@@ -61,9 +60,9 @@ namespace System
         public static IList<TEntity> Get<TEntity>(this TEntity entity, Expression<Func<TEntity, bool>> expression = null, int? takeCount = null, Installation installation = Installation.Default)
             where TEntity : INoSql
            => GetAsync(entity, expression, takeCount, installation).ConfigureAwait(false).GetAwaiter().GetResult();
-#warning Fare la stessa cosa fatta per AggregatedData
+
         public static string GetName<TEntity>(this TEntity entity, Installation installation = Installation.Default)
             where TEntity : INoSql
-        => Manager(entity.GetType()).GetName(installation);
+        => Manager<TEntity>().GetName(installation);
     }
 }
