570f816151d93b401a148c4231bc4453c16a71a5
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-05-03 15:46:37 +0200

    sealed attributes

--- Rystem.Interfaces/Conversion/CsvIgnore.cs
+++ Rystem.Interfaces/Conversion/CsvIgnore.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Rystem.Conversion
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1813:Avoid unsealed attributes", Justification = "<Pending>")]
-    public class CsvIgnore : Attribute
+    public sealed class CsvIgnore : Attribute
     {
-    }
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1813:Avoid unsealed attributes", Justification = "<Pending>")]
-    public class CsvProperty : Attribute
-    {
-        public string Name { get; }
-        public CsvProperty(string name)
-            => this.Name = name;
     }
 }
---
+++ Rystem.Interfaces/Conversion/CsvProperty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rystem.Conversion
+{
+    public sealed class CsvProperty : Attribute
+    {
+        public string Name { get; }
+        public CsvProperty(string name)
+            => this.Name = name;
+    }
+}
