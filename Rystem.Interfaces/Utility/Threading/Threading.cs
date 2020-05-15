d6a54dcf82d44a221f404cbe2336d3b1ad423f22
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-05-16 00:04:11 +0200

    Threading

---
+++ Rystem.Interfaces/Utility/Threading/Threading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Rystem.Utility
+{
+    public class Threading
+    {
+        public static bool ConfigureThread(int minWorker, int minIOC)
+            => ThreadPool.SetMinThreads(minWorker, minIOC);
+        public static bool ConfigureThread(ThreadingType type)
+        {
+            ThreadPool.GetMinThreads(out int minWorker, out int minIOC);
+            ThreadPool.GetMaxThreads(out int maxWorker, out int maxIOC);
+            switch (type)
+            {
+                case ThreadingType.Proxy:
+                    minWorker = maxWorker / 2;
+                    minIOC = maxIOC;
+                    break;
+                case ThreadingType.HeavyCpu:
+                    minIOC = maxIOC;
+                    break;
+                case ThreadingType.HeavyIO:
+                    minWorker = maxWorker / 2;
+                    break;
+            }
+            return ConfigureThread(minWorker, minIOC);
+        }
+    }
+}
---
+++ Rystem.Interfaces/Utility/Threading/ThreadingType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rystem.Utility
+{
+    public enum ThreadingType
+    {
+        Proxy,
+        HeavyCpu,
+        HeavyIO
+    }
+}
