c552529e4099f6536a4c93defa1f6889fb9d43ed
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-07-07 23:22:57 +0200

    sql

--- Rystem/Telemetry/Integrations/SqlParser.cs
+++ Rystem/Telemetry/Integrations/SqlParser.cs
@@ -2,6 +2,7 @@
 using Rystem.Aggregation;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,13 @@ namespace Rystem
     internal class SqlParser : IAggregationParser<Telemetry>
     {
         private readonly TelemetryConfiguration TelemetryConfiguration;
+        private SqlConnection NewConnection() => new SqlConnection(this.TelemetryConfiguration.ConnectionString);
         public SqlParser(TelemetryConfiguration telemetryConfiguration)
             => this.TelemetryConfiguration = telemetryConfiguration;
+
         public async Task ParseAsync(string queueName, IList<Telemetry> events, ILogger log, Installation installation)
         {
-            throw new NotImplementedException();
+            using SqlConnection sqlConnection = this.NewConnection();
         }
     }
 }
