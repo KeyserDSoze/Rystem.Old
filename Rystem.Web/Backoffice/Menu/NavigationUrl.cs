d8640115a9debb1cf81e1753576fc7a0f6353cb6
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-07-09 23:52:34 +0200

    Rystem.Web 1.1.12

--- Rystem.Web/Backoffice/Menu/NavigationUrl.cs
+++ Rystem.Web/Backoffice/Menu/NavigationUrl.cs
@@ -11,11 +11,13 @@ namespace Rystem.Web.Backoffice
         public string Action { get; }
         public string Controller { get; }
         public string Area { get; }
-        public NavigationUrl(string controller = "Home", string action = "Index", string area = "")
+        public IDictionary<string, string> Parameters { get; }
+        public NavigationUrl(string controller = "Home", string action = "Index", string area = "", IDictionary<string, string> parameters = null)
         {
             this.Action = action;
             this.Controller = controller;
             this.Area = area;
+            this.Parameters = parameters;
         }
     }
 }
