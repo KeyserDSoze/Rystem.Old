f187ae26f3c44fa615936089612c6dff0ac1bd36
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-11-29 21:09:43 +0100

    WebRequest migliorato

--- Rystem.Interfaces/RepositoryPattern/RepositoryPatternCallerExtensions.cs
+++ Rystem.Interfaces/RepositoryPattern/RepositoryPatternCallerExtensions.cs
@@ -24,33 +24,33 @@ namespace System
                 return default;
             }
         }
-        public static Task<TModel> GetAsync<TModel>(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, HttpHeaders headers = null)
+        public static Task<TModel> GetAsync<TModel>(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, IDictionary<string, string> headers = null)
             => CallAsync(repositoryCaller,
                 () => new Uri($"{repositoryCaller.Uri}/{id}").InvokeHttpAsync<TModel>(timeout, HttpMethod.Get, headers));
-        public static Task<IEnumerable<TModel>> ListAsync<TModel>(this IRepositoryCaller repositoryCaller, int timeout = 30000, HttpHeaders headers = null)
+        public static Task<IEnumerable<TModel>> ListAsync<TModel>(this IRepositoryCaller repositoryCaller, int timeout = 30000, IDictionary<string, string> headers = null)
             => CallAsync(repositoryCaller,
                 () => repositoryCaller.Uri.InvokeHttpAsync<IEnumerable<TModel>>(timeout, HttpMethod.Get, headers));
-        public static Task CreateAsync<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, HttpHeaders headers = null)
+        public static Task CreateAsync<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, IDictionary<string, string> headers = null)
             => CallAsync(repositoryCaller,
                 () => repositoryCaller.Uri.InvokeHttpAsync(timeout, HttpMethod.Post, headers, "application/json", entity.ToJson()));
-        public static Task UpdateAsync<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, HttpHeaders headers = null)
+        public static Task UpdateAsync<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, IDictionary<string, string> headers = null)
             => CallAsync(repositoryCaller,
                 () => repositoryCaller.Uri.InvokeHttpAsync(timeout, HttpMethod.Patch, headers, "application/json", entity.ToJson()));
-        public static Task DeleteAsync(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, HttpHeaders headers = null)
+        public static Task DeleteAsync(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, IDictionary<string, string> headers = null)
             => CallAsync(repositoryCaller,
                 () => new Uri($"{repositoryCaller.Uri}/{id}").InvokeHttpAsync(timeout, HttpMethod.Delete, headers));
     }
     public static partial class RepositoryPatternCallerExtensions
     {
-        public static TModel Get<TModel>(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, HttpHeaders headers = null)
+        public static TModel Get<TModel>(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, IDictionary<string, string> headers = null)
             => repositoryCaller.GetAsync<TModel>(id, timeout, headers).ToResult();
-        public static IEnumerable<TModel> List<TModel>(this IRepositoryCaller repositoryCaller, int timeout = 30000, HttpHeaders headers = null)
+        public static IEnumerable<TModel> List<TModel>(this IRepositoryCaller repositoryCaller, int timeout = 30000, IDictionary<string, string> headers = null)
             => repositoryCaller.ListAsync<TModel>(timeout, headers).ToResult();
-        public static void Create<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, HttpHeaders headers = null)
+        public static void Create<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, IDictionary<string, string> headers = null)
             => repositoryCaller.CreateAsync(entity, timeout, headers).ToResult();
-        public static void Update<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, HttpHeaders headers = null)
+        public static void Update<TModel>(this IRepositoryCaller repositoryCaller, TModel entity, int timeout = 30000, IDictionary<string, string> headers = null)
             => repositoryCaller.UpdateAsync(entity, timeout, headers).ToResult();
-        public static void Delete(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, HttpHeaders headers = null)
+        public static void Delete(this IRepositoryCaller repositoryCaller, string id, int timeout = 30000, IDictionary<string, string> headers = null)
             => repositoryCaller.DeleteAsync(id, timeout, headers).ToResult();
     }
 }
--- Rystem.Interfaces/Utility/Net/HttpWebRequestExtensions.cs
+++ Rystem.Interfaces/Utility/Net/HttpWebRequestExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Rystem;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -9,15 +10,15 @@ namespace System.Net
 {
     public static partial class HttpWebRequestExtensions
     {
-        public static async Task<string> InvokeHttpAsync(this Uri uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, string body = null)
-            => await uri.InvokeHttpAsyncWithStream(timeout, method, headers, contentType, new MemoryStream(Encoding.UTF8.GetBytes(body)));
-        public static async Task<string> InvokeHttpAsyncWithStream(this Uri uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, Stream body = null)
+        public static async Task<string> InvokeHttpAsync(this Uri uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string,string> headers = null, string contentType = null, string body = null)
+            => await uri.InvokeHttpAsyncWithStream(timeout, method, headers, contentType, body != null ? new MemoryStream(Encoding.UTF8.GetBytes(body)) : null);
+        public static async Task<string> InvokeHttpAsyncWithStream(this Uri uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, Stream body = null)
         {
             using (HttpWebResponse httpWebResponse = await uri.PrepareRequest(timeout, method, headers, contentType, body))
             using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
                 return await streamReader.ReadToEndAsync().NoContext();
         }
-        private static async Task<HttpWebResponse> PrepareRequest(this Uri uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, Stream body = null)
+        private static async Task<HttpWebResponse> PrepareRequest(this Uri uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, Stream body = null)
         {
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
             httpWebRequest.Timeout = timeout;
@@ -42,7 +43,7 @@ namespace System.Net
             }
             return (HttpWebResponse)(await httpWebRequest.GetResponseAsync().NoContext());
         }
-        public static async Task<(HttpWebResponse Response, byte[] Body)> InvokeHttpToByteAsync(this Uri uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, Stream body = null)
+        public static async Task<(HttpWebResponse Response, byte[] Body)> InvokeHttpToByteAsync(this Uri uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, Stream body = null)
         {
             HttpWebResponse httpWebResponse = await uri.PrepareRequest(timeout, method, headers, contentType, body);
             using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
@@ -56,24 +57,24 @@ namespace System.Net
                 return (httpWebResponse, bytes);
             }
         }
-        public static async Task<string> InvokeHttpPostAsync<TEntity>(this TEntity entity, Uri uri, int timeout = 30_000, HttpHeaders headers = null)
+        public static async Task<string> InvokeHttpPostAsync<TEntity>(this TEntity entity, Uri uri, int timeout = 30_000, IDictionary<string, string> headers = null)
             => await uri.InvokeHttpAsync(timeout, HttpMethod.Post, headers, "application/json; charset=UTF-8", entity.ToDefaultJson()).NoContext();
 
-        public static async Task<TEntity> InvokeHttpAsync<TEntity>(this Uri uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, string body = null)
+        public static async Task<TEntity> InvokeHttpAsync<TEntity>(this Uri uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, string body = null)
            => (await uri.InvokeHttpAsync(timeout, method, headers, contentType, body).NoContext()).FromDefaultJson<TEntity>();
-        public static async Task<TResponse> InvokeHttpPostAsync<TEntity, TResponse>(this TEntity entity, Uri uri, int timeout = 30_000, HttpHeaders headers = null)
+        public static async Task<TResponse> InvokeHttpPostAsync<TEntity, TResponse>(this TEntity entity, Uri uri, int timeout = 30_000, IDictionary<string, string> headers = null)
             => (await entity.InvokeHttpPostAsync(uri, timeout, headers).NoContext()).FromDefaultJson<TResponse>();
     }
     public static partial class HttpWebRequestExtensions
     {
-        public static async Task<string> InvokeHttpAsync(this string uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, string body = null)
+        public static async Task<string> InvokeHttpAsync(this string uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, string body = null)
             => await new Uri(uri).InvokeHttpAsync(timeout, method, headers, contentType, body).NoContext();
-        public static async Task<string> InvokeHttpPostAsync<TEntity>(this TEntity entity, string uri, int timeout = 30_000, HttpHeaders headers = null)
+        public static async Task<string> InvokeHttpPostAsync<TEntity>(this TEntity entity, string uri, int timeout = 30_000, IDictionary<string, string> headers = null)
             => await entity.InvokeHttpPostAsync(new Uri(uri), timeout, headers).NoContext();
 
-        public static async Task<TEntity> InvokeHttpAsync<TEntity>(this string uri, int timeout = 30_000, HttpMethod method = null, HttpHeaders headers = null, string contentType = null, string body = null)
+        public static async Task<TEntity> InvokeHttpAsync<TEntity>(this string uri, int timeout = 30_000, HttpMethod method = null, IDictionary<string, string> headers = null, string contentType = null, string body = null)
           => (await uri.InvokeHttpAsync(timeout, method, headers, contentType, body).NoContext()).FromDefaultJson<TEntity>();
-        public static async Task<TResponse> InvokeHttpPostAsync<TEntity, TResponse>(this TEntity entity, string uri, int timeout = 30_000, HttpHeaders headers = null)
+        public static async Task<TResponse> InvokeHttpPostAsync<TEntity, TResponse>(this TEntity entity, string uri, int timeout = 30_000, IDictionary<string, string> headers = null)
             => (await entity.InvokeHttpPostAsync(uri, timeout, headers).NoContext()).FromDefaultJson<TResponse>();
     }
 }
